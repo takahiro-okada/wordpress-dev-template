738ef435645f092f88612b3f42268691410196ea
takahiro-okada <okahanako09875@gmail.com>
2023-01-03 07:02:05 +0900

    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Header
+ */
+
+?>
+
+<?php get_template_part('template-parts/header/header-meta'); ?>
+
+<body <?php body_class(); ?><?php get_template_part('template-parts/header/header-content'); ?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Index
+ */
+
+get_header();
+?>
+<main>
+  Main
+</main>
+<footer>
+  footer
+</footer>
+</body>
+
+</html>
