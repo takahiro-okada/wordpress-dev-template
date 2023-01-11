9d62fa2964aa720df5c8715366750a955b349d46
takahiro-okada <okahanako09875@gmail.com>
2023-01-11 11:45:54 +0900

    feat:Add compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 const helloGulp = (done) => {
   console.log("Heelo!!");
   done();
 };
 
+const compileSass = (done) => {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./dist/css"));
+  done();
+};
+
 exports.test = helloGulp;
+exports.compileSass = compileSass;
