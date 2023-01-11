74e2464e3bb484129fdb9365de62523fc848ab2b
takahiro-okada <okahanako09875@gmail.com>
2023-01-11 15:15:52 +0900

    feat: Add hot reload

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,23 +1,44 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync");
+
+const srcPath = "./src/scss/**/*.scss";
+const distPath = "../dist/css";
 
 const helloGulp = (done) => {
   console.log("Heelo!!");
   done();
 };
 
-const compileSass = (done) => {
-  return gulp
-    .src("./src/scss/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("../dist/css"));
-  done();
+const compileSass = () => {
+  return gulp.src(srcPath).pipe(sass()).pipe(gulp.dest(distPath));
 };
 
 const watch = () => {
-  gulp.watch("./src/scss/**/*.scss", compileSass);
+  gulp.watch("./src/scss/**/*.scss", gulp.series(compileSass, browserReload));
+};
+
+const browserInit = (done) => {
+  browserSync.init({
+    files: ["**/*"],
+    // 静的サイト
+    // server: {
+    //   baseDir: "./",
+    // },
+    // 動的サイト
+    proxy: "http://wordpressdevtemplate.local/",
+    opne: true,
+  });
+  done();
+};
+
+const browserReload = (done) => {
+  browserSync.reload();
+  done();
 };
 
 exports.test = helloGulp;
 exports.compileSass = compileSass;
 exports.watch = watch;
+exports.browserInit = browserInit;
+exports.dev = gulp.parallel(browserInit, watch);
